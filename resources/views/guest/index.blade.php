509d17c6349e015f78cb15912906842f11bb29b4
Karl Jarren Macadangdang <kj.macadangdang@mylogoinc.com>
2022-06-17 09:29:58 +0800

    Added banner label

--- resources/views/guest/index.blade.php
+++ resources/views/guest/index.blade.php
@@ -1,32 +1,40 @@
 @extends('layouts.guest')
 
 @section('content')
-  <div id="banner" class="carousel slide" data-ride="carousel">
-    <div class="carousel-inner">
-      <div class="carousel-item active">
-        <div class="d-flex align-items-center h-100 w-100">
-          <img class="d-block w-100" src="{{ asset('assets/img/carousel1.jpg') }}" alt="First slide">
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="d-flex align-items-center h-100 w-100">
-          <img class="d-block w-100" src="{{ asset('assets/img/carousel2.jpg') }}" alt="Second slide">
-        </div>
-      </div>
-      <div class="carousel-item">
-        <div class="d-flex align-items-center h-100 w-100">
-          <img class="d-block w-100" src="{{ asset('assets/img/carousel3.jpg') }}" alt="Third slide">
-        </div>
+  <div class="position-relative">
+    <div class="d-flex flex-column align-items-center justify-content-center position-absolute h-100 w-100" style="z-index: 1;">
+      <div style="background-color: rgba(0, 0, 0, 0.5); border-radius: 1rem; max-width: 50%;">
+        <img src="{{ asset('assets/img/sp-logo.svg') }}" class="box-logo">
+        <div class="display-5 text-white text-center pb-4" style="text-shadow: 2px 4px #477035;">Bringing Nature to Every Home</div>
       </div>
     </div>
-    <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev">
-      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="carousel-control-next" href="#banner" role="button" data-slide="next">
-      <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only">Next</span>
-    </a>
+    <div id="banner" class="carousel slide" data-ride="carousel">
+      <div class="carousel-inner">
+        <div class="carousel-item active">
+          <div class="d-flex align-items-center h-100 w-100">
+            <img class="d-block w-100" src="{{ asset('assets/img/carousel1.jpg') }}" alt="First slide">
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="d-flex align-items-center h-100 w-100">
+            <img class="d-block w-100" src="{{ asset('assets/img/carousel2.jpg') }}" alt="Second slide">
+          </div>
+        </div>
+        <div class="carousel-item">
+          <div class="d-flex align-items-center h-100 w-100">
+            <img class="d-block w-100" src="{{ asset('assets/img/carousel3.jpg') }}" alt="Third slide">
+          </div>
+        </div>
+      </div>
+      <a class="carousel-control-prev" href="#banner" role="button" data-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+      </a>
+      <a class="carousel-control-next" href="#banner" role="button" data-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+      </a>
+    </div>
   </div>
   <div class="container py-5">
     <div class="row align-items-center">
